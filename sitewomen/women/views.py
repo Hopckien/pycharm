f7da15f9f9f9dbe7ba6da267bda6543b8cc15b64
Hopckien <hop.mad@gmail.com>
2024-12-10 22:28:10 +0300

    Работа с несвязанными формами

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,49 +1,66 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import (
+    HttpResponse,
+    HttpResponseNotFound,
+    Http404,
+    HttpResponseRedirect,
+    HttpResponsePermanentRedirect,
+)
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
+from .forms import AddPostForm
 from .models import Women, Category, TagPost
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
+menu = [
+    {"title": "О сайте", "url_name": "about"},
+    {"title": "Добавить статью", "url_name": "add_page"},
+    {"title": "Обратная связь", "url_name": "contact"},
+    {"title": "Войти", "url_name": "login"},
 ]
 
 
 def index(request):
-    posts = Women.published.all().select_related('cat')
+    posts = Women.published.all().select_related("cat")
 
     data = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': 0,
+        "title": "Главная страница",
+        "menu": menu,
+        "posts": posts,
+        "cat_selected": 0,
     }
-    return render(request, 'women/index.html', context=data)
+    return render(request, "women/index.html", context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
+    return render(request, "women/about.html", {"title": "О сайте", "menu": menu})
 
 
 def show_post(request, post_slug):
     post = get_object_or_404(Women, slug=post_slug)
 
     data = {
-        'title': post.title,
-        'menu': menu,
-        'post': post,
-        'cat_selected': 1,
+        "title": post.title,
+        "menu": menu,
+        "post": post,
+        "cat_selected": 1,
     }
 
-    return render(request, 'women/post.html', data)
+    return render(request, "women/post.html", data)
 
 
 def addpage(request):
-    return HttpResponse("Добавление статьи")
+    # return HttpResponse("Добавление статьи")
+    if request.method == "POST":
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = AddPostForm()
+
+    data = {"menu": menu, "title": "Добавление статьи", "form": form}
+    return render(request, "women/addpage.html", data)
 
 
 def contact(request):
@@ -59,12 +76,12 @@ def show_category(request, cat_slug):
     posts = Women.published.filter(cat_id=category.pk).select_related("cat")
 
     data = {
-        'title': f'Рубрика: {category.name}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': category.pk,
+        "title": f"Рубрика: {category.name}",
+        "menu": menu,
+        "posts": posts,
+        "cat_selected": category.pk,
     }
-    return render(request, 'women/index.html', context=data)
+    return render(request, "women/index.html", context=data)
 
 
 def page_not_found(request, exception):
@@ -76,11 +93,10 @@ def show_tag_postlist(request, tag_slug):
     posts = tag.tags.filter(is_published=Women.Status.PUBLISHED).select_related("cat")
 
     data = {
-        'title': f"Тег: {tag.tag}",
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': None,
+        "title": f"Тег: {tag.tag}",
+        "menu": menu,
+        "posts": posts,
+        "cat_selected": None,
     }
 
-    return render(request, 'women/index.html', context=data)
-
+    return render(request, "women/index.html", context=data)
